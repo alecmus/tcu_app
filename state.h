d9779ebc1fe5eaaff1614a786e2d42600bbbd7be
Alec T. Musasa <alecmus@live.com>
2021-05-01 22:08:51 +0200

    Updated license.

--- state.h
+++ state.h
@@ -1,9 +1,28 @@
 /*
 ** Top Class Uniforms Application State Interface (state.h)
 **
-** This code may not be copied, modified or distributed without the
-** express written permission of the author(s). Any violation shall
-** be prosecuted to the maximum extent possible under law.
+*************************************************************************
+** MIT License
+**
+** Copyright (c) 2019 Mthandazo J.J. Ncube, Ropafadzo Siziba, Takura Chimoyo, Kennedy J.J. Maturure,  Alec T. Musasa, Wallis T. Makurumidze
+**
+** Permission is hereby granted, free of charge, to any person obtaining a copy
+** of this softwareand associated documentation files(the "Software"), to deal
+** in the Software without restriction, including without limitation the rights
+** to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
+** copies of the Software, and to permit persons to whom the Software is
+** furnished to do so, subject to the following conditions :
+**
+** The above copyright noticeand this permission notice shall be included in all
+** copies or substantial portions of the Software.
+**
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+** IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+** FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+** AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+** LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+** OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+** SOFTWARE.
 **
 *************************************************************************
 ** Project Details:
@@ -11,7 +30,6 @@
 ** National University of Science and Technology
 ** SCS2104 Structured System Analysis and Design 2019
 ** Group 4: Mthandazo, Ropafadzo, Takura, Kennedy, Alec, Wallis
-**
 */
 
 #pragma once
