58636a8512de1e9b18ab55a0c9e5c4a80d61a931
Alec T. Musasa <alecmus@live.com>
2021-05-01 21:37:03 +0200

    Added version information.

---
+++ version_info.h
@@ -0,0 +1,30 @@
+/*
+** Top Class Uniforms Version Information (version_info.h)
+**
+** This code may not be copied, modified or distributed without the
+** express written permission of the author(s). Any violation shall
+** be prosecuted to the maximum extent possible under law.
+**
+*************************************************************************
+** Project Details:
+**
+** National University of Science and Technology
+** SCS2104 Structured System Analysis and Design 2019
+** Group 4: Mthandazo, Ropafadzo, Takura, Kennedy, Alec, Wallis
+**
+*/
+
+#pragma once
+
+#define appname			"TCU Order App"
+#define appdescription	"Top Class Uniforms Order Management System"
+#define appcomments		"Powered by the cui framework"
+#define appcopyright	""
+#define appfilename		"tcu_app.exe"
+
+#define appversion		"1.0.0"
+#define appmajor		1
+#define appminor		0
+#define apprevision		0
+
+#define appdate			"25 Mar 2020"
